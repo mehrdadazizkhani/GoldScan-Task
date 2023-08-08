4c223365136ba94c757685f38955ad74b34980e0
Mehrdad Azizkhani <mehrdad.azizkhani@gmail.com>
2023-08-08 03:32:16 +0330

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-[100dvh] w-full bg-slate-500 p-4">
+      <section className="container m-auto flex w-full gap-3 bg-red-400"></section>
+    </main>
+  );
+}
+
+export default App;
